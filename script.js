84a50a8913b2973bea85e305f1b3f531471f2b27
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-09-01 18:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 let draggedDiv = null;
 
 document.querySelectorAll(".image").forEach(div => {
+  // Drag start
   div.addEventListener("dragstart", function () {
     draggedDiv = this;
     this.classList.add("selected");
   });
 
+  // Drag end
   div.addEventListener("dragend", function () {
     this.classList.remove("selected");
   });
 
+  // Allow drop
   div.addEventListener("dragover", function (e) {
     e.preventDefault();
   });
 
+  // Swap background images
   div.addEventListener("drop", function (e) {
     e.preventDefault();
     if (draggedDiv !== this) {
-      let parent = this.parentNode;
-      let draggedNext = draggedDiv.nextSibling;
-      let droppedNext = this.nextSibling;
-
-      parent.insertBefore(draggedDiv, droppedNext);
-      parent.insertBefore(this, draggedNext);
+      let temp = this.style.backgroundImage;
+      this.style.backgroundImage = draggedDiv.style.backgroundImage;
+      draggedDiv.style.backgroundImage = temp;
     }
   });
 });
